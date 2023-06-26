d991857c71cef1827b050d204e130d57f49626d3
unknown <juanignacio.delossantos@4rsoluciones.com>
2023-06-26 12:22:32 -0300

    started the project and add some pages, navbar and layout

---
+++ app/components/navbar.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full py-3 px-5 flex items-center justify-between border-b border-gray-800">
+      <h1 className={`text-white text-xl font-bold`}>Products App</h1>
+      <ul className="w-2/6 flex items-center justify-between">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/dashboard">Dashboard</Link>
+        </li>
+        <li>
+          <Link href="/login">Login</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+export default Navbar;
