98f360d00e02424358804294e383e71abc0e6a9b
unknown <juanignacio.delossantos@4rsoluciones.com>
2023-06-27 10:50:11 -0300

    change NavBarComponent to NavBar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-import Navbar from "./components/NavBarComponent";
+import Navbar from "./components/NavBar";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
